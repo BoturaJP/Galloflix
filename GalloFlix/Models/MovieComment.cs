cf6f4c684d2480c490b72235766bdacd1133201b
BoturaJP <rafinhabotura@gmail.com>
2023-08-01 11:48:47 -0300

    Começamos a corrigir e alterar o Galloflix

--- GalloFlix/Models/MovieComment.cs
+++ GalloFlix/Models/MovieComment.cs
@@ -7,6 +7,7 @@ namespace GalloFlix.Models
     public class MovieComment
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
